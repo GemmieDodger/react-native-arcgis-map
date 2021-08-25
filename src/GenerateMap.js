0d8b021e35ae5124683c942a79bb24fb4dde1775
gemmiedodger <gemmadruce@live.co.uk>
2021-08-25 20:08:21 +0100

    making sure repo is up-to-date with the idisplay and package.json

--- src/GenerateMap.js
+++ src/GenerateMap.js
@@ -6,6 +6,7 @@ export default function GenerateMap() {
     return (
       <WebView
         source={{uri: 'https://geaspoeclvheq4bv.maps.arcgis.com/apps/mapviewer/index.html?webmap=f34aa3bfc29a4574bb9cc017895c073a'}}
-        style={{marginTop: 20, height: 50}}
+        style={{marginTop: 20, height: 50, width: 380}}
+        UseInAppAuth={true}
       />);
 }
